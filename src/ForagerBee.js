e70b43377eaea298c8b3bf2bd63df97b8083008f
taha0505 <taha.khelfa@hotmail.fr>
2020-03-20 19:08:52 +0100

    Implement 6ees6ees6ees using ES6 js

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor(){
+    super();
+    this.age =10 ;
+    this.canFly = true ;
+    this.treasureChest =[] ;
+    this.job = 'find pollen'
+  }
+  forage(treasure){
+    this.treasureChest.push(treasure)
+  }
 };
